a008c857247a9a086b6cce13f92d05cf8af96ec1
Carmen Cojocariu <cojocariucarmen@gmail.com>
2017-09-16 15:21:56 -0700

    Finish devise and omni, consider just omni

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:twitter]
+
+   def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end

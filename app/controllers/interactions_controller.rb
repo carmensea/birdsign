6dbf0e71de60879c25a3d6b908750211340b8949
Kevin Weaver <kevinweaver2@gmail.com>
2017-09-16 17:32:34 -0700

    file upload js

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -6,7 +6,16 @@ class InteractionsController < ApplicationController
     @interaction = Interaction.new
   end
 
+  def create
+    @interaction = Interaction.new(interaction_params)
+  end
+
   private
+
+    def interaction_params
+      params.require(:interaction).permit(:id, :photo_url)
+    end
+
   	def set_s3_direct_post
   	  @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   	end

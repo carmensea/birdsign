198b4d1b3bcd971e78fb9dba474e7828f509a922
Kevin Weaver <kevinweaver2@gmail.com>
2017-09-16 17:12:00 -0700

    Added file upload to view

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916230505) do
+ActiveRecord::Schema.define(version: 20170917000200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "interactions", force: :cascade do |t|
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
